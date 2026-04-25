fde60b68eba8d7d79e8fcf33e5541f3ff66cea99
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-04-25 15:12:44 +0545

    remove unnessary jq dependency by writing query logic in python

--- logparser.py
+++ logparser.py
@@ -3,14 +3,13 @@
 import re
 import json
 import argparse
-import jq
 
 logFile = './parsed-log.json'
 errorsFile = './errors.json'
 
 parser = argparse.ArgumentParser(
-        description=("A commandline utility that parses syslogd logs." 
-                     "It produces two \'.json\' files \'errors.json\' and \'parsed-logs.json\'." 
+        description=("A commandline utility that parses syslogd logs. " 
+                     "It produces two \'.json\' files \'errors.json\' and \'parsed-logs.json\'. " 
                      "The files contain json object list of parsed logs and json object" 
                      "list of parsed logs with erros respectively."),
         prog="logparser"
@@ -24,32 +23,20 @@ source = args.source
 searchProgram = args.search
 
 
+# Regex: 1 = timestamp, 2 = hostname, 3 = program, 4 = PID (optional), 5 = message
 def logParser():
-    syslog = source
-
-    # Regex:
-    # 1 = timestamp
-    # 2 = hostname
-    # 3 = program
-    # 4 = PID (optional)
-    # 5 = message
-
     reobj = re.compile(
         r"(^[a-z]{3}\s+[0-9]+\s+[0-9:]+)"
         r"\s+([a-z0-9-]+)\s+([a-z0-9_.-]+)"
         r"(?:\[(\d+)])?:\s+(.*)$",
         re.I
     )
-
-    parsed_logs = []
-
-    with open(syslog, 'r', encoding="utf-8") as syslg:
+    with open(source, 'r', encoding="utf-8") as syslg:
+        parsed_logs = []
         for line in syslg:
             reggy = reobj.search(line)
-            
             if not reggy:
                 continue
-
             parsed_logs.append({
                 'Timestamp': reggy.group(1),
                 'Hostname': reggy.group(2),
@@ -57,14 +44,11 @@ def logParser():
                 'PID': reggy.group(4),
                 'Message': reggy.group(5)
                 })
-
-    error_logs = []
-
-    for logs in parsed_logs:
-        for k,v in logs.items():
-            if v == None: continue
-            if "error" in v.lower(): error_logs.append(logs)
-
+        error_logs = []
+        for logs in parsed_logs:
+            for k,v in logs.items():
+                if v == None: continue
+                if "error" in v.lower(): error_logs.append(logs)
     return ( parsed_logs, error_logs )
 
 
@@ -72,29 +56,32 @@ def writeToFile():
     gLogs = logParser()
     with open(logFile, 'w+', encoding="utf-8") as pars:
         json.dump(gLogs[0], pars, indent=4)
-    
     with open(errorsFile, 'w+', encoding="utf-8") as errf:
         json.dump(gLogs[1], errf, indent=4)
 
 
 def logSearch():
-    with open(errorsFile, "r", encoding="utf-8") as errs2:
-        errors2 = json.load(errs2)
-    messages = jq.compile('.[] | select ( .Program | test($program; "i"))', args={"program": searchProgram[0] })\
-            .input(errors2).all()
-    print(json.dumps(messages))
+    eLogs = logParser()
+    pErrorLogs = []
+    for logs in eLogs[1]:
+        if logs["Program"].lower() == searchProgram[0].lower():
+            pErrorLogs.append(logs)
+    print(json.dumps(pErrorLogs))
+
 
 def printLogs():
     gLogs = logParser()
     print(json.dumps(gLogs[0]))
 
+
 def printErrLogs():
     gLogs = logParser()
     print(json.dumps(gLogs[1]))
 
-if searchProgram: 
-    logParser()
-    logSearch()
-else:
-    printLogs()
-    writeToFile()
+
+if __name__ == "__main__":
+    if searchProgram: 
+        logSearch()
+    else: 
+        printLogs()
+
