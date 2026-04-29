4d351c705dae46ae94d91117158f1cc9fa82d6c8
Anuranjan Gyawali <anuranjangyawali930@proton.me>
2026-04-29 11:08:12 +0545

    make writing to files optional & document it

--- README.md
+++ README.md
@@ -6,10 +6,20 @@ Make sure the script is executeable first:
 chmod +x ./logparser
 ```
 
-By default, without any flags, it prints valid JSON to the stdout and also creates .json log files in the current directory. 
+By default, without any flags, it prints valid JSON logs to stdout.
 
 ```shell
 ./logparser
+```
+With -o [PATH] optional argument you can specify the directory to write the log files.
+
+```shell
+./logparser -o $HOME
+```
+With -f [program] logparser prints error logs of the specified program to stdout.
+
+```shell
+./logparser -f kernel
 ```
 
 For help enter:
--- logparser.py
+++ logparser.py
@@ -3,20 +3,23 @@
 import re
 import json
 import argparse
-
+import os
 
 def arg_parse():
     parser = argparse.ArgumentParser(
             description=("A commandline utility that parses syslogd logs. " 
-                         "It produces two \'.json\' files \'errors.json\' and \'parsed-logs.json\'. " 
-                         "The files contain json object list of parsed logs and json object" 
-                         "list of parsed logs with erros respectively."),
+                         "It produces two \'.json\' files \'errors.json\' and \'logs.json\'. " 
+                         "The files contain json object list of all parsed logs and json object" 
+                         "list of parsed errors logs respectively."),
             prog="logparser"
             )
 
-    parser.add_argument("source", nargs="?" , help="path to your syslog file", type=str , default="/var/log/syslog")
-    parser.add_argument("--error","-e", action="store_true", help="outputs only error logs", dest='isset')
-    parser.add_argument("--search","-s", help="outputs error logs of a specific program only", dest='program')
+    parser.add_argument("source", nargs="?" , help="Syslog file path", type=str , default="/var/log/syslog")
+    parser.add_argument("--error", "-e", help="output error logs only", action="store_true", dest='isset_e')
+    parser.add_argument("--find", "-f", help="output error logs of a specific program only", dest='program')
+    parser.add_argument("--output", "-o", help="write logs to the specified directory", nargs='?',\
+            const="./", dest='path')
+
     args = parser.parse_args() 
 
     return args
@@ -54,15 +57,23 @@ def log_parser():
     return (parsed_logs, error_logs)
 
 
-def write_to_file():
-    log_file = './parsed-log.json'
-    errors_file = './errors.json'
+def write_to_file(path):
+    log_file = "logs.json"
+    err_file = "erros.json"
+
+    if not os.path.isdir(path):
+        log_file = path.__add__(log_file)
+        err_file = path.__add__(err_file)
+    elif os.path.isdir(path):
+        log_file = path.__add__('/' + log_file)
+        err_file = path.__add__('/' + err_file)
 
     get_logs = log_parser()
-    with open(log_file, 'w+', encoding="utf-8") as pars:
+    with open(log_file, "w+", encoding="utf-8") as pars:
         json.dump(get_logs[0], pars, indent=4)
-    with open(errors_file, 'w+', encoding="utf-8") as errf:
+    with open(err_file, "w+", encoding="utf-8") as errf:
         json.dump(get_logs[1], errf, indent=4)
+
 
 
 def log_search():
@@ -87,6 +98,7 @@ def print_err_logs():
 if __name__ == "__main__":
     args = arg_parse()
     if args.program: log_search()
-    elif args.isset: print_err_logs()
-    else: print_logs(); write_to_file()
+    elif args.isset_e: print_err_logs()
+    elif args.path: write_to_file(args.path)
+    else: print_logs()
 
